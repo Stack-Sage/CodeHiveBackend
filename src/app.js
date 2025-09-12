25bc638c63caebca20b8666ece4b049f9cc5155b
Stack-Sage <adarsh.pathania.04@gmail.com>
2025-09-12 21:50:31 +0530

    writing login and register and logout

--- src/app.js
+++ src/app.js
@@ -15,6 +15,10 @@ app.use(express.static("public"))
 
 app.use(cookieParser())   
 
+app.use("/",(req,res)=>{
+   res.send("I'm just a Chill guy!!")
+})
+
 import { userRouter } from './routes/user.routes.js'
 
 app.use("/api/user",userRouter)
