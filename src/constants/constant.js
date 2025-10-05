6143506b2f2bd5361fe18a69ea36fb3ef25a8780
Stack-Sage <adarsh.pathania.04@gmail.com>
2025-10-05 21:42:05 +0530

    data model updates

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,20 +1,15 @@
 export const accessTokenOptions = {
   httpOnly: true,
   secure: process.env.COOKIE_SECURE === 'true',
-  sameSite: process.env.COOKIE_SAME_SITE || 'lax',
+  sameSite: process.env.COOKIE_SAME_SITE || 'none',
   maxAge: 24 * 60 * 60 * 1000, 
 };
 
-export const refreshTokenOptions = {
-  httpOnly: true,
-  secure: process.env.COOKIE_SECURE === 'true',
-  sameSite: process.env.COOKIE_SAME_SITE || 'lax',
-  maxAge: 10 * 24 * 60 * 60 * 1000, 
-};
+
 
 export const options = {
   httpOnly: true,
   secure: process.env.COOKIE_SECURE === 'true',
-  sameSite: process.env.COOKIE_SAME_SITE || 'lax',
+  sameSite: process.env.COOKIE_SAME_SITE || 'none',
   maxAge: 24 * 60 * 60 * 1000,
 };
