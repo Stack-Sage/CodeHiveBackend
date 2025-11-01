094a0596f2c3dcc2ef32dcaa8283e7575ff51a10
Stack-Sage <adarsh.pathania.04@gmail.com>
2025-11-01 14:42:41 +0530

    contants updates

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,13 +1,13 @@
 export const accessTokenOptions = {
   httpOnly: true,
-  secure: false, // should be boolean, not string
-  sameSite: 'lax',
+  secure: true,
+  sameSite: 'none',
   maxAge: 7 * 24 * 60 * 60 * 1000, 
 };
 
 export const options = {
   httpOnly: true,
-  secure: false, // should be boolean, not string
-  sameSite: 'lax',
+  secure: true, 
+  sameSite: 'none',
   maxAge: 7 * 24 * 60 * 60 * 1000,
 };
